46f4d60b2cb7c8c0c339e939106ae47f7a5a1e94
Dmytro Mishchuk <dmytro.mishchuk@sap.com>
2018-12-17 15:50:35 +0200

    read/write files

--- modules/dirwatcher.js
+++ modules/dirwatcher.js
@@ -1,13 +1,18 @@
 'use strict';
 const fs = require('fs');
 const eventEmitter = require('./eventEmitter');
+let watchDelay;
 
 class DirWatcherthat {
     constructor(dir, delay) {
-        fs.watch(dir, {
-            aggregateTimeout: delay
-        }, (eventType, filename) => {
-            eventEmitter.emit('changed', filename);
+        fs.watch(dir, null, (eventType, filename) => {
+            if (!watchDelay) {
+                eventEmitter.emit('changed', {
+                    filename: filename.split('___jb_tmp___')[0],
+                    dir
+                });
+                watchDelay = setTimeout(function() { watchDelay = null }, delay);
+            }
         });
         console.log('DirWatcher module');
     }
--- modules/importer.js
+++ modules/importer.js
@@ -1,10 +1,26 @@
 'use strict';
+const fs = require('fs');
 const eventEmitter = require('./eventEmitter');
+const csv = require('csvtojson');
+const cTable = require('console.table');
 
 class Importer {
     constructor() {
-        eventEmitter.on('changed', (filename) => {
-            console.log('changed');
+        eventEmitter.on('changed', ({ dir, filename }) => {
+            const path = dir ?  `${dir}/${filename}`:`./${filename}`;
+            fs.readFile(path, 'utf8', function (err, csvStr) {
+                if ( err ) throw err;
+                csv({
+                    noheader:false,
+                    output: "json"
+                })
+                    .fromString(csvStr)
+                    .then((data)=>{
+                        const table = cTable.getTable(data);
+                        console.log(table);
+                        console.log(`Reading ${filename} successfully completed...`);
+                    });
+            });
         });
     }
 }
