adff46f56152ab214c6ea0f1564c18c93c9cdb21
Dmytro Mishchuk <dmytro.mishchuk@sap.com>
2018-12-17 17:02:34 +0200

    final

--- modules/importer.js
+++ modules/importer.js
@@ -7,17 +7,35 @@ const cTable = require('console.table');
 class Importer {
     constructor() {
         eventEmitter.on('changed', (path) => {
+            this.import(path).then((data) => {
+                console.log('Async method:\n');
+                const table = cTable.getTable(data);
+                console.log(table);
+            });
+
+            csv({ noheader: false, output: "json" })
+                .fromString(this.importSync(path))
+                .then(data => {
+                    console.log('Sync method:\n');
+                    const table = cTable.getTable(data);
+                    console.log(table);
+                });
+        });
+    }
+    import(path) {
+        return new Promise((resolve, reject) => {
             fs.readFile(path, 'utf8', function (err, csvStr) {
-                if ( err ) throw err;
+                if ( err ) reject(err);
                 csv({ noheader: false, output: "json" })
                     .fromString(csvStr)
-                    .then((data)=>{
-                        const table = cTable.getTable(data);
-                        console.log(table);
-                        console.log(`Reading ${filename} successfully completed...`);
+                    .then(data => {
+                        resolve(data);
                     });
             });
         });
+    }
+    importSync(path) {
+        return fs.readFileSync(path, 'utf8');
     }
 }
 
