ed642bcc3957dc9910f1981a0547de8ed25259a7
Dmytro Mishchuk <dmytro.mishchuk@sap.com>
2019-01-24 12:37:50 +0200

    homework3 - part 3

--- utils/streams.js
+++ utils/streams.js
@@ -1,17 +1,57 @@
 const fs = require('fs');
 const program = require('commander');
 
+function reverse(str) {
+    console.log(str);
+}
+function transform(str) { /* ... */ }
+function outputFile(filePath) {
+    console.log(filePath)
+}
+function convertFromFile(filePath) { /* ... */ }
+function convertToFile(filePath) { /* ... */ }
+
 program
-    .option('-a, --actions [action]', 'Set action', (action) => {
-        console.log(action);
-    })
-    .option('-f, --file [filename]', 'Set filename', (filename) => {
-        console.log(filename);
+    .option('-f, --file [filename]', 'Set filename')
+    .option('-a, --action [action]', 'Set action', (action) => {
+        const actions = {
+            reverse: {
+                arg: 'str',
+                func: reverse
+            },
+            transform: {
+                arg: 'str',
+                func: transform
+            },
+            outputFile: {
+                arg: 'filePath',
+                func: outputFile
+            },
+            convertFromFile: {
+                arg: 'filePath',
+                func: convertFromFile
+            },
+            convertToFile: {
+                arg: 'filePath',
+                func: convertToFile
+            },
+        };
+        if(!actions[action]) {
+            console.log('Unknown type of action passed, please try again');
+        } else {
+            setTimeout(() => {
+                const arg = actions[action]['arg'] === 'str' ? program.args[0] : program.file;
+                if(!arg) {
+                    console.log('Wrong input');
+                    return;
+                }
+                actions[action]['func'](arg);
+            }, 1000);
+        }
     });
 
-program.command('*').action(() => {
-    console.log('Wrong input, please check the instruction');
-    program.help();
+program.command('*').action((command) => {
+    console.log(command);
 });
 
 program.parse(process.argv);
@@ -21,8 +61,3 @@ if(process.argv.length == 2) {
     program.help();
 }
 
-function reverse(str) { /* ... */ }
-function transform(str) { /* ... */ }
-function outputFile(filePath) { /* ... */ }
-function convertFromFile(filePath) { /* ... */ }
-function convertToFile(filePath) { /* ... */ }
